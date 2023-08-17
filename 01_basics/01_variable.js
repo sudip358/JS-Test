52e9ccc8b826e7a2e77aa46dee194628b7a13d36
sudip358 <contact@edumap.io>
2023-08-17 18:25:06 +0000

    varaible

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 14453
+let accountEmail = "s@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur" 
+
+//accountId = 2 //
+
+accountEmail = "a@gmail.com"
+accountPassword = "32"
+accountCity = "banglore"
+
+console.log("accountId")
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+/prefer not to use var/
