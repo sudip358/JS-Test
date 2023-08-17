6dc46e11d71671457e5688dd009be75d46ca1eaf
sudip358 <contact@edumap.io>
2023-08-17 18:22:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// alert(3+3) we are using nodejs not browser
+
+console.log(3+3);
+console.log("hites");
+
+
+// number
+// bigint
+// string
+// boolean true false
+// null
+// undefined
+// symbol
+
+
+// object
+
+console.log(typeof"hi");
