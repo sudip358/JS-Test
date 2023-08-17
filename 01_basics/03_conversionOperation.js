5dc516a985b8e58679257f27a262fa57617a27fa
sudip358 <contact@edumap.io>
2023-08-17 19:15:22 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,3 +7,14 @@ console.log(typeof valueInNumber)
 
 
 //
+
+let str1 = "hi"
+let str2 = "hi2"
+str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+20);
+
+console.log("1"+"2");
+console.log(1+2+1);
+console.log("1"+"2");
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,5 @@
+console.log(2>1); number number
+console.log("2">1); string number
+console.log(null > 0); // null gets converted to zero
+
+console.log(undefined > 0);
