0dd04ed83090343729024090003ea25c9ed9dee8
sudip358 <contact@edumap.io>
2023-08-18 16:54:39 +0000

    updated with ecma

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,5 +1,11 @@
+
+
+
 console.log(2>1); number number
 console.log("2">1); string number
 console.log(null > 0); // null gets converted to zero
 
 console.log(undefined > 0);
+
+// === strict check datatype check
+console.log("2" === 2);
---
+++ datatypes_summary.js
@@ -0,0 +1,40 @@
+// primitite -call by value  -  7 types
+//string number boolean null undefined symol bigint 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+// symbol usage
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id === anotherId);
+const bigNumber = 211151n
+
+
+
+
+// master objects and webevents
+
+//non primitive - call by reference
+
+// array objects functions array objects functions
+
+const heros = ["shak", "nagraj", "doga"]
+let myObj = { 
+    name: "hit",
+    age: 22,
+
+}
+
+const myFunction = function() {
+console.log("hello");    
+}
+
+console.log(typeof bigNumber);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
