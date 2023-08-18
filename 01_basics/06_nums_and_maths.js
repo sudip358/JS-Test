f583ea5c5523dc777bf58dbbb9ad1e5f514e0124
sudip358 <contact@edumap.io>
2023-08-18 18:10:29 +0000

    number maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score = 400
+
+const balance = new Number(100.112)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+
+
+const otherNumber = 23.8966
+//console.log(otherNumber.toPrecision(3));
+
+
+const hunders = 1000000
+//console.log(hunders.toLocaleString('en-IN'));
+
+
+// +++++++++++++++ Maths   ++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+
+const min = 10
+const max = 20
+
+
+
+
+// important line
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)
+
+
+
