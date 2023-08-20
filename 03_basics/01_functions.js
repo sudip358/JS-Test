bcfeba7011f13701bb4567867cc635e36eeea7cf
sudip358 <contact@edumap.io>
2023-08-20 11:00:03 +0000

    fun

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -58,7 +58,44 @@ function loginUserMessage(username){
  //function loginUserMessage(username: any): String
 //console.log(loginUserMessage("hi"))
 
-console.log(loginUserMessage())
+//console.log(loginUserMessage())
+
+
+
+//... rest operator
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 500, 600));
+
+const user = {
+    username: "hi",
+    price: "199"
+}
+
+function handleObject(anyobject){
+    console.log(`user is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+//handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
+
 
 
 
